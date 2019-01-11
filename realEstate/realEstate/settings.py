d2cc4ea20430e9f0bec4ba32de1da7cf0f602e50
Patrik <patriknilsson1992@gmail.com>
2019-01-11 08:56:24 +0700

    connect postgres

--- realEstate/realEstate/settings.py
+++ realEstate/realEstate/settings.py
@@ -70,13 +70,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'realEstate.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'real-estate',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432 # default postgres port
     }
 }
 
